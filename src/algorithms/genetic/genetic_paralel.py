137d58703e5811cd8ba83aec470200c3ca4d022c
mrsuiii <emeryfathanzwageri@gmail.com>
2024-11-03 15:32:22 +0700

    feat :: add oop in ga

--- src/algorithms/genetic/genetic_paralel.py
+++ src/algorithms/genetic/genetic_paralel.py
@@ -1,139 +1,215 @@
 import numpy as np
-import sys
-import os
-import time
 import random
+import time
 from concurrent.futures import ThreadPoolExecutor
+import os
+import sys
 
-# Add the parent directory of 'algorithms' to sys.path
+# Menambahkan path untuk mengimpor fungsi eksternal
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
-# Now you can import from utils
 from utils.count_of_match import count_of_match
-from utils.crossover import *
-n = 5
-magic_number = 315
+from utils.crossover import custom_randomized_segment_preserving_crossover, custom_probabilistic_randomized_segment_preserving_crossover
 
-# generating initial population
-def generate_initial_population(size=100) -> list:
-    population = []
-    for _ in range(size):
-        individual = np.random.permutation(n**3) + 1
-        population.append(individual)
-    return population
+class GA_solver:
+    def __init__(self, crossover_func="randomized", population_size=100, iterations=1000, shuffle=False):
+        """
+        Inisialisasi solver GA.
 
-# count score of each individual in population
-def count_score(population) -> list:
-    scores = []
-    for individual in population:
-        scores.append(count_of_match(individual))
-    return scores
+        Parameters:
+            crossover_func (str): Jenis crossover function yang akan digunakan ("randomized" atau "probabilistic").
+            population_size (int): Jumlah individu dalam populasi.
+            iterations (int): Jumlah iterasi algoritma genetika.
+            shuffle (bool): Apakah populasi baru akan diacak setelah setiap iterasi.
+        """
+        self.n = 5
+        self.magic_number = 315
+        self.crossover_func = crossover_func
+        self.shuffle = shuffle
+        self.population_size = population_size
+        self.iterations = iterations
+        self.initial_population = self.generate_initial_population(population_size)
 
-# selection function from population to choice parent with weighted weights
-def selection(population, scores, k=1) -> list:
-    weights = scores if np.sum(scores) > 0 else None
-    selected_population = random.choices(population, weights=weights, k=k)
-    return selected_population[0]
+    def generate_initial_population(self, size=100) -> list:
+        """
+        Membuat populasi awal dengan elemen unik.
 
-# mutate function using random mutation
-def mutate(individual):
-    idx1, idx2 = random.sample(range(n**3), 2)
-    individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
-    return individual
+        Parameters:
+            size (int): Jumlah individu dalam populasi.
 
-# Cycle Crossover (CX)
-def cycle_crossover(parent1, parent2):
-    child1 = [-1] * len(parent1)
-    child2 = [-1] * len(parent2)
-    
-    def create_cycle(child, parent1, parent2):
-        index = 0
-        while index < len(child) and child[index] != -1:
-            index += 1
+        Returns:
+            list: Populasi awal yang terdiri dari individu-individu.
+        """
+        population = []
+        for _ in range(size):
+            individual = np.random.permutation(self.n ** 3) + 1
+            population.append(individual)
+        return population
+
+    def find_best_individual(self, population):
+        """
+        Menemukan individu terbaik dalam populasi berdasarkan skor.
+
+        Parameters:
+            population (list): Daftar individu dalam populasi.
+
+        Returns:
+            np.array: Individu dengan skor tertinggi dalam populasi.
+        """
+        scores = self.count_score(population)
+        best_index = np.argmax(scores)
+        return population[best_index]
+
+    def count_score(self, population) -> list:
+        """
+        Menghitung skor untuk setiap individu dalam populasi.
+
+        Parameters:
+            population (list): Daftar individu dalam populasi.
+
+        Returns:
+            list: Daftar skor untuk setiap individu.
+        """
+        scores = []
+        for individual in population:
+            scores.append(count_of_match(individual))
+        return scores
+
+    def selection(self, population, scores, k=1) -> list:
+        """
+        Memilih individu dari populasi dengan weighted selection.
+
+        Parameters:
+            population (list): Daftar individu dalam populasi.
+            scores (list): Daftar skor untuk setiap individu.
+            k (int): Jumlah individu yang dipilih.
+
+        Returns:
+            list: Individu yang terpilih dari populasi.
+        """
+        weights = scores if np.sum(scores) > 0 else None
+        selected_population = random.choices(population, weights=weights, k=k)
+        return selected_population[0]
+
+    def mutate(self, individual):
+        """
+        Melakukan mutasi pada individu dengan menukar dua elemen acak.
+
+        Parameters:
+            individual (np.array): Individu yang akan dimutasi.
+
+        Returns:
+            np.array: Individu yang telah dimutasi.
+        """
+        idx1, idx2 = random.sample(range(self.n ** 3), 2)
+        individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
+        return individual
+
+    def genetic_algorithm(self):
+        """
+        Menjalankan algoritma genetika untuk menemukan solusi terbaik.
+
+        Returns:
+            dict: Informasi konfigurasi akhir, indeks terbaik, dan skor iterasi.
+        """
+        best_score = 0
+        population = self.initial_population
+        iteration_scores = []
+        for iter in range(self.iterations):
+            scores = self.count_score(population)
+            individual_with_best_score = np.argmax(scores)
+            best_score = scores[individual_with_best_score]
+            # Simpan skor terbaik untuk setiap iterasi
+            iteration_scores.append((best_score, individual_with_best_score))
+            if (iter + 1) % 10 == 0:
+                print(f"Best score in {iter + 1}-iterations: {best_score} state ke: {individual_with_best_score}")
+
+            new_population = []
+            with ThreadPoolExecutor() as executor:
+                # Membuat anak-anak secara paralel
+                futures = [
+                    executor.submit(self.create_children, population, scores, self.crossover_func, iter, self.iterations)
+                    for _ in range(self.population_size // 2)
+                ]
+                for future in futures:
+                    child1, child2 = future.result()
+                    new_population.extend([child1, child2])
+
+            if self.shuffle:
+                random.shuffle(new_population)
+            population = new_population
+
+        final_solution_index = np.argmax(scores)
+        final_solution = population[final_solution_index]
         
-        if index == len(child):
-            return
+        solution = {
+            "config": final_solution,
+            "index": final_solution_index,
+            "iteration_scores": iteration_scores
+        }
+        return solution
+
+    def create_children(self, population, scores, crossover_func, iter, total_iteration):
+        """
+        Membuat anak-anak dari dua induk menggunakan crossover dan mutasi.
+
+        Parameters:
+            population (list): Daftar individu dalam populasi.
+            scores (list): Daftar skor untuk setiap individu.
+            crossover_func (str): Jenis crossover yang akan digunakan.
+            iter (int): Iterasi saat ini.
+            total_iteration (int): Total iterasi yang akan dijalankan.
+
+        Returns:
+            tuple: Dua anak yang dihasilkan dari crossover dan mutasi.
+        """
+        parent1 = self.selection(population, scores)
+        parent2 = self.selection(population, scores)
+
+        # Pilih crossover function berdasarkan crossover_func
+        if crossover_func == "randomized":
+            child1, child2 = custom_randomized_segment_preserving_crossover(parent1, parent2)
+        else:
+            child1, child2 = custom_probabilistic_randomized_segment_preserving_crossover(parent1, parent2, iterations=total_iteration, current_iteration=iter)
+
+        # Mutasi pada anak-anak dengan probabilitas 10%
+        if random.random() < 0.1:
+            child1 = self.mutate(child1)
+        if random.random() < 0.1:
+            child2 = self.mutate(child2)
+
+        return child1, child2
+
+    def solve(self) -> dict:
+        """
+        Menjalankan algoritma genetika dan memberikan hasil solusi.
+
+        Returns:
+        dict: Dictionary dengan informasi solusi berdasarkan opsi yang dipilih dalam `isi`, berisi elemen berikut:
         
-        start = index
-        while True:
-            child[index] = parent1[index]
-            index_array = np.where(parent1 == parent2[index])[0]
-            if len(index_array) == 0:
-                break
-            index = index_array[0]
-            if index == start:
-                break
+            - "elapsed_time" (float): Waktu yang dibutuhkan untuk menjalankan algoritma dalam detik.
+            - "best_initial_state" (np.array): Individu terbaik dalam populasi awal.
+            - "best_index" (int): Indeks individu terbaik pada akhir iterasi.
+            - "best_config" (np.array): Konfigurasi terbaik yang ditemukan oleh algoritma genetika.
+            - "iteration_scores" (list of tuples): Daftar skor terbaik di setiap iterasi dalam bentuk tuple, di mana setiap tuple terdiri dari:
+                - score (int): Skor terbaik pada iterasi tersebut.
+                - index (int): Indeks individu terbaik dalam populasi pada iterasi tersebut.
+        """
+        start_time = time.time()
+        solution = self.genetic_algorithm()
+        end_time = time.time()
+        elapsed_time = end_time - start_time  # Hitung waktu eksekusi
 
-    create_cycle(child1, parent1, parent2)
-    create_cycle(child2, parent2, parent1)
+        initial_population = self.initial_population
+        best_initial_individual = self.find_best_individual(initial_population)
+        index = solution['index']
+        best_config = solution['config']
+        iteration_scores = solution['iteration_scores']
 
-    for i in range(len(child1)):
-        if child1[i] == -1:
-            child1[i] = parent2[i]
-        if child2[i] == -1:
-            child2[i] = parent1[i]
-
-    return child1, child2
-
-
-import numpy as np
-
-
-# genetic algorithm with parallel processing
-def genetic_algorithm(fitness_fun, iterations=1000, population_size=100, shuffle = False):
-    best_score = 0
-    population = generate_initial_population(size=population_size)
-
-    for iter in range(iterations):
-        scores = count_score(population)
-        individual_with_best_score = np.argmax(scores)
-        best_score = scores[individual_with_best_score]
-        if (iter+1)%10 ==0 :
-            print(f"Best score in {iter+1}-iterations: {best_score} populasi ke: {individual_with_best_score}")
-        
-        new_population = []
-        with ThreadPoolExecutor() as executor:
-            # Generate children in parallel
-            futures = [
-                executor.submit(create_children, population, scores, fitness_fun,iter,iterations)
-                for _ in range(population_size // 2)
-            ]
-            for future in futures:
-                child1, child2 = future.result()
-                new_population.extend([child1, child2])
-        
-        if shuffle == True:
-            random.shuffle(new_population)
-        population = new_population
-
-    final_solution_index = np.argmax(scores)
-    final_solution = population[final_solution_index]
-    print("konfigurasi: ", final_solution)
-    print("score akhir: ", best_score)
-    return final_solution
-
-def create_children(population, scores, fitness_fun,iter,total_iteration):
-    parent1 = selection(population, scores)
-    parent2 = selection(population, scores)
-    child1, child2 = fitness_fun(parent1, parent2,iterations =total_iteration, current_iteration = iter)
-    
-    if random.random() < 0.15:
-        child1 = mutate(child1)
-    if random.random() < 0.15:
-        child2 = mutate(child2)
-    
-    return child1, child2
-
-start_time = time.time()
-
-final_solution = genetic_algorithm(custom_probabilistic_randomized_segment_preserving_crossover, 8000, 400,shuffle=True)
-end_time = time.time()
-if(len(final_solution) == len(np.unique(final_solution))):
-    print(len(final_solution))
-    print("ARRAY UNIK")
-else:
-    print("Salah cok")
-
-print("Elapsed time: {:.6f} seconds".format(end_time - start_time))
-
-print(find_segments_with_magic_number(final_solution))
+        solution = {
+            "elapsed_time": elapsed_time,
+            "best_initial_state": best_initial_individual,
+            "best_index": index,
+            "best_config": best_config,
+            "iteration_scores": iteration_scores
+        }
+        return solution
--- src/utils/crossover.py
+++ src/utils/crossover.py
@@ -1,26 +1,23 @@
 import random
 import numpy as np
 
-def custom_randomized_segment_preserving_crossover(parent1, parent2,iterations = None, current_iteration = None):
-    n = int(round(len(parent1) ** (1/3)))  
-    all_elements = set(range(1, n**3 + 1)) 
+def custom_randomized_segment_preserving_crossover(parent1, parent2):
+    
     child1 = [-1] * len(parent1)  
     child2 = [-1] * len(parent2)  
     
     protected_indices_p1 = find_protected_indices(parent1)
     protected_indices_p2 = find_protected_indices(parent2)
     
-    # Pertahankan segmen yang sudah benar di masing-masing child
+    
     for i in protected_indices_p1:
         child1[i] = parent1[i]
     for i in protected_indices_p2:
         child2[i] = parent2[i]
-    
-    # Ambil elemen dari parent lain yang belum digunakan di child
+
     unused_elements_child1 = [parent2[i] for i in range(len(parent2)) if parent2[i] not in child1]
     unused_elements_child2 = [parent1[i] for i in range(len(parent1)) if parent1[i] not in child2]
     
-    # Tempatkan elemen-elemen yang belum digunakan secara acak di indeks yang kosong
     fill_randomly(child1, unused_elements_child1)
     fill_randomly(child2, unused_elements_child2)
 
@@ -31,27 +28,22 @@ def fill_randomly(child, unused_elements):
     random.shuffle(unused_elements)  
     missing_elements = iter(unused_elements)
     
-    # Isi tempat kosong dalam child dengan elemen yang tidak digunakan
     for i in range(len(child)):
         if child[i] == -1: 
             child[i] = next(missing_elements)
 
-import random
-import random
-
 def custom_probabilistic_randomized_segment_preserving_crossover(parent1, parent2, initial_prob=0.0001, peak_prob=0.01, final_prob=0.0001, iterations=1000, current_iteration=1, prob_unprotected=1.0):
-    # Hitung nilai `prob_protected` berdasarkan parabola yang mencapai puncaknya di tengah iterasi
+    
     midpoint = iterations / 8
     if current_iteration <= midpoint:
         prob_protected = initial_prob + (peak_prob - initial_prob) * (current_iteration / midpoint)
     else:
         prob_protected = peak_prob - (peak_prob - final_prob) * ((current_iteration - midpoint) / midpoint)
-        # prob_protected = final_prob
-    n = int(round(len(parent1) ** (1/3)))  # Ukuran sisi kubus, misalnya 5 untuk kubus 5x5x5
-    child1 = [-1] * len(parent1)  # Inisialisasi dengan -1
-    child2 = [-1] * len(parent2)  # Inisialisasi dengan -1
+       
+    n = int(round(len(parent1) ** (1/3)))  
+    child1 = [-1] * len(parent1)  
+    child2 = [-1] * len(parent2) 
 
-    # Temukan segmen-segmen yang mencapai magic number pada parent1 dan parent2
     protected_indices_p1 = find_protected_indices(parent1)
     protected_indices_p2 = find_protected_indices(parent2)
     
@@ -62,11 +54,9 @@ def custom_probabilistic_randomized_segment_preserving_crossover(parent1, parent
         if i in protected_indices_p2 and random.random() > prob_protected:  # Gunakan > untuk mempertahankan
             child2[i] = parent2[i]
 
-    # Ambil elemen dari parent lain yang belum digunakan di child
     unused_elements_child1 = [parent2[i] for i in range(len(parent2)) if parent2[i] not in child1]
     unused_elements_child2 = [parent1[i] for i in range(len(parent1)) if parent1[i] not in child2]
     
-    # Isi elemen yang masih kosong untuk menjaga keunikan
     fill_remaining_elements_random(child1, unused_elements_child1)
     fill_remaining_elements_random(child2, unused_elements_child2)
 
@@ -74,14 +64,13 @@ def custom_probabilistic_randomized_segment_preserving_crossover(parent1, parent
 
 def fill_remaining_elements_random(child, unused_elements):
     """Mengisi elemen yang kosong di anak dengan elemen yang tidak digunakan secara acak."""
-    random.shuffle(unused_elements)  # Acak urutan elemen yang akan dimasukkan
-    used_elements = set(child) - {-1}  # Elemen yang sudah digunakan di child, abaikan -1
-    missing_elements = [elem for elem in unused_elements if elem not in used_elements]  # Elemen yang belum ada di child
+    random.shuffle(unused_elements)  
+    used_elements = set(child) - {-1} 
+    missing_elements = [elem for elem in unused_elements if elem not in used_elements] 
     missing_elements = iter(missing_elements)
     
-    # Isi tempat kosong dalam child dengan elemen yang tidak digunakan
     for i in range(len(child)):
-        if child[i] == -1:  # Anggap -1 sebagai posisi kosong
+        if child[i] == -1:  
             child[i] = next(missing_elements, None)
             if child[i] is None:
                 raise ValueError("Tidak cukup elemen unik untuk mengisi semua posisi kosong dalam child.")
@@ -90,26 +79,22 @@ def find_protected_indices(arr):
     """Mengembalikan indeks dari segmen yang mencapai magic number."""
     protected_indices = set()
     
-    # Row Count (x-axis)
     for i in range(25):
         sumRow = arr[5*i] + arr[5*i + 1] + arr[5*i + 2] + arr[5*i + 3] + arr[5*i + 4]
         if sumRow == 315:
             protected_indices.update([5*i, 5*i + 1, 5*i + 2, 5*i + 3, 5*i + 4])
 
-    # Column Count (y-axis)
     for i in range(5):
         for j in range(5):
             sumColumn = arr[i*25 + j] + arr[i*25 + 5 + j] + arr[i*25 + 10 + j] + arr[i*25 + 15 + j] + arr[i*25 + 20 + j]
             if sumColumn == 315:
                 protected_indices.update([i*25 + j, i*25 + 5 + j, i*25 + 10 + j, i*25 + 15 + j, i*25 + 20 + j])
 
-    # Pillar Count (z-axis)
     for i in range(25):
         sumPillar = arr[i] + arr[i + 25] + arr[i + 50] + arr[i + 75] + arr[i + 100]
         if sumPillar == 315:
             protected_indices.update([i, i + 25, i + 50, i + 75, i + 100])
 
-    # Diagonal of xy-plane Count
     for i in range(5):
         sumDiagonal1 = arr[25*i] + arr[25*i + 6] + arr[25*i + 12] + arr[25*i + 18] + arr[25*i + 24]
         if sumDiagonal1 == 315:
@@ -119,7 +104,6 @@ def find_protected_indices(arr):
         if sumDiagonal2 == 315:
             protected_indices.update([25*i + 4, 25*i + 8, 25*i + 12, 25*i + 16, 25*i + 20])
 
-    # Diagonal of xz-plane Count
     for i in range(5):
         sumDiagonal1 = arr[5*i] + arr[5*i + 26] + arr[5*i + 52] + arr[5*i + 78] + arr[5*i + 104]
         if sumDiagonal1 == 315:
@@ -129,7 +113,6 @@ def find_protected_indices(arr):
         if sumDiagonal2 == 315:
             protected_indices.update([5*i + 4, 5*i + 28, 5*i + 52, 5*i + 76, 5*i + 100])
 
-    # Diagonal of yz-plane Count
     for i in range(5):
         sumDiagonal1 = arr[i] + arr[i + 30] + arr[i + 60] + arr[i + 90] + arr[i + 120]
         if sumDiagonal1 == 315:
@@ -139,7 +122,6 @@ def find_protected_indices(arr):
         if sumDiagonal2 == 315:
             protected_indices.update([i + 20, i + 40, i + 60, i + 80, i + 100])
 
-    # Space Diagonal Count
     spaceDiagonal1 = arr[0] + arr[31] + arr[62] + arr[93] + arr[124]
     spaceDiagonal2 = arr[20] + arr[41] + arr[62] + arr[83] + arr[104]
     spaceDiagonal3 = arr[4] + arr[33] + arr[62] + arr[91] + arr[120]
@@ -158,29 +140,26 @@ def find_protected_indices(arr):
 
 
 def find_segments_with_magic_number(arr, magic_number=315):
-    n = int(round(len(arr) ** (1/3)))  # Ukuran sisi kubus
+    n = int(round(len(arr) ** (1/3)))  
     segments = {"rows": [], "columns": [], "pillars": [], "xy_diagonals": [], "xz_diagonals": [], "yz_diagonals": [], "space_diagonals": []}
 
-    # Row Count (x-axis)
     for i in range(25):
         sumRow = arr[5*i] + arr[5*i + 1] + arr[5*i + 2] + arr[5*i + 3] + arr[5*i + 4]
         if sumRow == magic_number:
             segments["rows"].append([5*i, 5*i + 1, 5*i + 2, 5*i + 3, 5*i + 4])
 
-    # Column Count (y-axis)
     for i in range(5):
         for j in range(5):
             sumColumn = arr[i*25 + j] + arr[i*25 + 5 + j] + arr[i*25 + 10 + j] + arr[i*25 + 15 + j] + arr[i*25 + 20 + j]
             if sumColumn == magic_number:
                 segments["columns"].append([i*25 + j, i*25 + 5 + j, i*25 + 10 + j, i*25 + 15 + j, i*25 + 20 + j])
 
-    # Pillar Count (z-axis)
     for i in range(25):
         sumPillar = arr[i] + arr[i + 25] + arr[i + 50] + arr[i + 75] + arr[i + 100]
         if sumPillar == magic_number:
             segments["pillars"].append([i, i + 25, i + 50, i + 75, i + 100])
 
-    # Diagonal of xy-plane Count
+    
     for i in range(5):
         sumDiagonal1 = arr[25*i] + arr[25*i + 6] + arr[25*i + 12] + arr[25*i + 18] + arr[25*i + 24]
         if sumDiagonal1 == magic_number:
@@ -190,7 +169,7 @@ def find_segments_with_magic_number(arr, magic_number=315):
         if sumDiagonal2 == magic_number:
             segments["xy_diagonals"].append([25*i + 4, 25*i + 8, 25*i + 12, 25*i + 16, 25*i + 20])
 
-    # Diagonal of xz-plane Count
+    
     for i in range(5):
         sumDiagonal1 = arr[5*i] + arr[5*i + 26] + arr[5*i + 52] + arr[5*i + 78] + arr[5*i + 104]
         if sumDiagonal1 == magic_number:
@@ -200,7 +179,6 @@ def find_segments_with_magic_number(arr, magic_number=315):
         if sumDiagonal2 == magic_number:
             segments["xz_diagonals"].append([5*i + 4, 5*i + 28, 5*i + 52, 5*i + 76, 5*i + 100])
 
-    # Diagonal of yz-plane Count
     for i in range(5):
         sumDiagonal1 = arr[i] + arr[i + 30] + arr[i + 60] + arr[i + 90] + arr[i + 120]
         if sumDiagonal1 == magic_number:
@@ -210,7 +188,6 @@ def find_segments_with_magic_number(arr, magic_number=315):
         if sumDiagonal2 == magic_number:
             segments["yz_diagonals"].append([i + 20, i + 40, i + 60, i + 80, i + 100])
 
-    # Space Diagonal Count
     spaceDiagonal1 = arr[0] + arr[31] + arr[62] + arr[93] + arr[124]
     spaceDiagonal2 = arr[20] + arr[41] + arr[62] + arr[83] + arr[104]
     spaceDiagonal3 = arr[4] + arr[33] + arr[62] + arr[91] + arr[120]
@@ -226,3 +203,37 @@ def find_segments_with_magic_number(arr, magic_number=315):
         segments["space_diagonals"].append([24, 43, 62, 81, 100])
 
     return segments
+
+
+def cycle_crossover(parent1, parent2):
+    child1 = [-1] * len(parent1)
+    child2 = [-1] * len(parent2)
+    
+    def create_cycle(child, parent1, parent2):
+        index = 0
+        while index < len(child) and child[index] != -1:
+            index += 1
+        
+        if index == len(child):
+            return
+        
+        start = index
+        while True:
+            child[index] = parent1[index]
+            index_array = np.where(parent1 == parent2[index])[0]
+            if len(index_array) == 0:
+                break
+            index = index_array[0]
+            if index == start:
+                break
+
+    create_cycle(child1, parent1, parent2)
+    create_cycle(child2, parent2, parent1)
+
+    for i in range(len(child1)):
+        if child1[i] == -1:
+            child1[i] = parent2[i]
+        if child2[i] == -1:
+            child2[i] = parent1[i]
+
+    return child1, child2
